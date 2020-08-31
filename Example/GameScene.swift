76e99dd92b465d24bc7cd47c654aea47fb3bbe1d
Raisat <ramazanova.raisat@yandex.ru>
2020-08-31 14:07:41 +0300

    Update GameScene.swift
    small code-style refactoring

--- Example/GameScene.swift
+++ Example/GameScene.swift
@@ -14,35 +14,62 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var backgroundNode: SKNode!
     var man: SKSpriteNode!
-    var scoreLabel:SKLabelNode!
+    var scoreLabel: SKLabelNode!
     
-    var score:Int = 0 {
-        didSet{
+    var score: Int = 0 {
+        didSet {
             scoreLabel.text = "Счет: \(score)"
         }
     }
     
-    var gameTimer:Timer!
-    var aliens = ["bird"]
+    var gameTimer: Timer!
+    var enemies = ["bird"]
     
-    let alienCategory:UInt32 = 0x1 << 1
-    let bulletCategory:UInt32 = 0x1 << 0
+    let enemyCategory: UInt32 = 0x1 << 1
+    let bulletCategory: UInt32 = 0x1 << 0
     
     override func didMove(to view: SKView) {
-                
+        configureScene()
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        man.physicsBody!.applyImpulse(CGVector(dx: 0, dy: 0.1))
+        fireBullet()
+    }
+
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+
+    func didBegin(_ contact: SKPhysicsContact) {
+        var enemyBody: SKPhysicsBody
+        var bulletBody: SKPhysicsBody
+
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            enemyBody = contact.bodyB
+            bulletBody = contact.bodyA
+        } else {
+            enemyBody = contact.bodyA
+            bulletBody = contact.bodyB
+        }
+
+        if (enemyBody.categoryBitMask & enemyCategory) != 0 &&
+            (bulletBody.categoryBitMask & bulletCategory) != 0 {
+            collisionElements(bulletNode: bulletBody.node as! SKSpriteNode, alianNode: enemyBody.node as! SKSpriteNode)
+        }
+    }
+
+    private func configureScene() {
         backgroundNode = self.childNode(withName: "background")!
         man = self.childNode(withName: "man") as? SKSpriteNode
-        
+
         let moveBackground = SKAction.move(by: CGVector(dx: -500, dy: 0), duration: 10)
-        
         backgroundNode.run(moveBackground)
-        
-        
+
         self.physicsWorld.contactDelegate = self
-        
         self.physicsWorld.gravity = CGVector(dx: 0, dy: -1)
         self.physicsWorld.contactDelegate = self
-        
+
         scoreLabel = SKLabelNode(text: "Счет: 0")
         scoreLabel.fontName = "AmericanTypewriter-Bold"
         scoreLabel.fontSize = 36
@@ -50,30 +77,15 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         scoreLabel.position = CGPoint(x: -200, y: 130)
         score = 0
         self.addChild(scoreLabel)
-        
-        gameTimer = Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(addAlien), userInfo: nil, repeats: true)
+
+        gameTimer = Timer.scheduledTimer(timeInterval: 3,
+                                         target: self,
+                                         selector: #selector(addEnemy),
+                                         userInfo: nil,
+                                         repeats: true)
     }
-    
-    func didBegin(_ contact: SKPhysicsContact) {
-        var alienBody:SKPhysicsBody
-        var bulletBody:SKPhysicsBody
-        
-        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
-            alienBody = contact.bodyB
-            bulletBody = contact.bodyA
-        }else {
-            alienBody = contact.bodyA
-            bulletBody = contact.bodyB
-        }
-        
-        if (alienBody.categoryBitMask & alienCategory) != 0 && (bulletBody.categoryBitMask & bulletCategory) != 0 {
-            collisionElements(bulletNode: bulletBody.node as! SKSpriteNode, alianNode: alienBody.node as! SKSpriteNode)
-        }
-    
-    }
-    
-    
-    func collisionElements(bulletNode:SKSpriteNode, alianNode:SKSpriteNode) {
+
+    private func collisionElements(bulletNode:SKSpriteNode, alianNode:SKSpriteNode) {
         let explosion = SKEmitterNode(fileNamed: "Fire.sks")
         explosion?.position = alianNode.position
         self.addChild(explosion!)
@@ -90,40 +102,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         score += 5
     }
     
-    @objc func addAlien () {
-        aliens = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: aliens) as! [String]
-        
-        let alien = SKSpriteNode(imageNamed: aliens[0])
-        let randomPos = GKRandomDistribution(lowestValue: 0, highestValue: 160)
-        let pos = CGFloat(randomPos.nextInt())
-        alien.position = CGPoint(x: 800, y: pos)
-        alien.setScale(0.07)
-        alien.physicsBody = SKPhysicsBody(rectangleOf: alien.size)
-        alien.physicsBody?.isDynamic = true
-        
-        alien.physicsBody?.categoryBitMask = alienCategory
-        alien.physicsBody?.contactTestBitMask = bulletCategory
-        alien.physicsBody?.collisionBitMask = 0
-        
-        self.addChild(alien)
-        
-        let animDuration:TimeInterval = 6
-        
-        var actions = [SKAction]()
-        actions.append(SKAction.move(to: CGPoint(x: -320, y: pos), duration: animDuration))
-        actions.append(SKAction.removeFromParent())
-        
-        alien.run(SKAction.sequence(actions))
-    }
-    
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        man.physicsBody!.applyImpulse(CGVector(dx: 0, dy: 0.1))
-        fireBullet()
-    }
-
-    
-    func fireBullet() {
+    private func fireBullet() {
         self.run(SKAction.playSoundFileNamed("vzryv.mp3", waitForCompletion: false))
         
         let bullet = SKSpriteNode(imageNamed: "2")
@@ -134,7 +113,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         bullet.physicsBody?.isDynamic = true
         
         bullet.physicsBody?.categoryBitMask = bulletCategory
-        bullet.physicsBody?.contactTestBitMask = alienCategory
+        bullet.physicsBody?.contactTestBitMask = enemyCategory
         bullet.physicsBody?.collisionBitMask = 0
         bullet.physicsBody?.usesPreciseCollisionDetection = true
         
@@ -147,10 +126,32 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         actions.append(SKAction.removeFromParent())
         
         bullet.run(SKAction.sequence(actions))
-
     }
     
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+    @objc func addEnemy () {
+        enemies = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: enemies) as! [String]
+
+        let enemy = SKSpriteNode(imageNamed: enemies[0])
+        let randomPos = GKRandomDistribution(lowestValue: 0, highestValue: 160)
+        let pos = CGFloat(randomPos.nextInt())
+        enemy.position = CGPoint(x: 800, y: pos)
+        enemy.setScale(0.07)
+        enemy.physicsBody = SKPhysicsBody(rectangleOf: enemy.size)
+        enemy.physicsBody?.isDynamic = true
+
+        enemy.physicsBody?.categoryBitMask = enemyCategory
+        enemy.physicsBody?.contactTestBitMask = bulletCategory
+        enemy.physicsBody?.collisionBitMask = 0
+
+        self.addChild(enemy)
+
+        let animDuration:TimeInterval = 6
+
+        var actions = [SKAction]()
+        actions.append(SKAction.move(to: CGPoint(x: -320, y: pos), duration: animDuration))
+        actions.append(SKAction.removeFromParent())
+
+        enemy.run(SKAction.sequence(actions))
     }
+
 }
